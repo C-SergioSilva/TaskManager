d365b0fe91f74be4f1be45b37b971e9ad0939a1f
carlos sergio <csergio120605@gmail.com>
2022-07-20 20:00:25 -0300

    iniciando a criptografia da senha do usuário

---
+++ TaskManagerAPI/Utils/MD5Utils.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace TaskManagerAPI.Utils
+{
+    public class MD5Utils
+    {
+        public static string CreateHashMD5(string input)
+        {
+            MD5 md5Hash = MD5.Create();
+            var bytes = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach(var byt in bytes)
+            {
+                stringBuilder.Append(byt.ToString("X2"));
+            }
+            return stringBuilder.ToString();
+        }
+        
+    }
+}
